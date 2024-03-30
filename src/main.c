99d3d57105e611a90a4fc3ba608872c45452a922
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-03-30 20:16:10 +0800

    initialize

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <raylib.h>
+#include <stdio.h>
+
+int main(void) {
+    InitWindow(300, 300, "Hello, World!");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    CloseWindow();
+}
